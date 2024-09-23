b3d1353e8d5c30e65fe055e71094d5a91c0b8878
EvgeniiShataev <shataev.evgenii@gmail.com>
2024-09-23 16:41:00 +0300

    Create ex 1,2,3 in git.py

---
+++ ex 1,2,3 in git.py
@@ -0,0 +1,35 @@
+#Задание 1
+integer_var = 5
+float_var = 3.5
+string_var = "Oleg"
+list_var = [4, 5, -7, 27 ]
+tuple_var = (52, "Jorick")
+dict_var = {"name" : "Egor", "body size" : 50}
+set_var = {1, 2, 3, 4, 5}
+bool_var = True
+print(integer_var)
+print(float_var)
+print(string_var)
+print(list_var)
+print(tuple_var)
+print(dict_var)
+print(set_var)
+print(bool_var)
+#Задание 2 
+name = "Evgenii"
+age = 17
+average_grade = 4.85
+favorite_movies = ["Один дома", "Легенда", "Джанго освобожденный"]
+personal_info = ("surname : Shataev", "school number : 319", "avg grade : 4.85")
+skils = {'Jumping' : 5, 'Swim' : 7}
+visited_countries = {'Thailand', 'China', 'Turkey'}
+has_driver_license = False
+#Задание 3
+CPU = "Intel Core i7-12700KF"
+volume_of_RAM_in_gigabytes = 32
+average_internet_speed_in_megabytes_per_second = 77.79
+Options = [CPU, volume_of_RAM_in_gigabytes, average_internet_speed_in_megabytes_per_second]
+print(Options)
+
+
+
